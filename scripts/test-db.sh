594c81dce71b5f2285bafd7bbb005f9f55d905a6
marneusz <mateusz.wojcik.ds@gmail.com>
2024-09-30 00:47:36 +0200

    Updated test script

--- scripts/test-db.sh
+++ scripts/test-db.sh
@@ -1,5 +1,16 @@
 #! /bin/bash
 
-docker exec -it postgres_db psql -U $POSTGRES_USER -d postgres -c "SELECT * from test_table;"
+# Test database connection and basic operations
 
-docker exec -it postgres_db psql -U $POSTGRES_USER -d $POSTGRES_DB -c "SELECT 1;"
+set -e
+
+# Load environment variables
+source .env
+
+# Test connection
+docker exec -it postgres_db psql -h localhost -U $POSTGRES_USER -d $POSTGRES_DB -c '\l'
+docker exec -it postgres_db psql -U $POSTGRES_USER -d postgres -c "SELECT * from test_table;"
+docker exec -it postgres_db psql -U $POSTGRES_USER -d $POSTGRES_DB -c "SELECT 1;" 
+
+# Run integration tests
+python tests/integration_tests.py
